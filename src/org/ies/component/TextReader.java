430b7ba3891d781ae39293a7e2f2b0b43a2185c1
aittor18 <rodriguezaitor678@gmail.com>
2025-02-03 14:28:02 +0100

    Continuar TextReader

---
+++ src/org/ies/component/TextReader.java
@@ -0,0 +1,12 @@
+package org.ies.component;
+
+import org.ies.model.Text;
+
+public class TextReader implements Reader<Text> {
+    @Override
+    public Text read() {
+        System.out.println("Introduce el tamaño del archivo: ");
+
+        return null;
+    }
+}
