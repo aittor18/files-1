e4b3f8168b1dda087a19a36e432eb31a0f71f647
aittor18 <rodriguezaitor678@gmail.com>
2025-02-06 13:18:16 +0100

    No creo que sea asi FileReader

--- src/org/ies/component/FileReader.java
+++ src/org/ies/component/FileReader.java
@@ -6,16 +6,44 @@ import java.util.Scanner;
 
 public class FileReader implements Reader<File> {
     Scanner scanner = new Scanner(System.in);
+    TextReader textReader = new TextReader();
+    AudioReader audioReader = new AudioReader();
+    ImageReader imageReader = new ImageReader();
 
     @Override
     public File read() {
-        System.out.println("Introduce el tamaño del archivo: ");
-        int size = scanner.nextInt();
-        scanner.nextLine();
+        int option;
+        menu();
+        return null;
+    }
 
-        System.out.println("Introduce la ruta donde se encuentra el archivo: ");
-        String path = scanner.nextLine();
+    private void menu() {
+        int option;
+        do {
+            System.out.println("<<<<Introduce por teclado tu elección>>>>");
+            System.out.println("1. Leer archivo de texto");
+            System.out.println("2. Leer archivo de audio");
+            System.out.println("3. Leer archivo de imágen");
+            System.out.println("4. Salir del menú");
+            System.out.print("Teclea tu opción: ");
+            option = scanner.nextInt();
+            scanner.nextLine();
 
-        return new File(size, path);
+            switch (option) {
+                case 1:
+                    textReader.read();
+                    break;
+                case 2:
+                    audioReader.read();
+                    break;
+                case 3:
+                    imageReader.read();
+                    break;
+                default:
+                    System.out.println("Opción inválida");
+
+            }
+        } while (option != 4);
     }
 }
+
