011bd406a2f6dcabd658f350f9d4880b7fb89cc1
aittor18 <rodriguezaitor678@gmail.com>
2025-02-10 10:34:14 +0100

    No entiendo nada

--- src/org/ies/Main.java
+++ src/org/ies/Main.java
@@ -1,10 +1,15 @@
 package org.ies;
 
-import org.ies.component.FileReader;
+import org.ies.component.Reader;
+import org.ies.component.TextReader;
+import org.ies.model.File;
+import org.ies.model.Program;
+import org.ies.model.Text;
 
 public class Main {
 
     public static void main(String[] args) {
-
+        var textReader = new TextReader();
+        var program = new Program<Text>();
     }
 }
